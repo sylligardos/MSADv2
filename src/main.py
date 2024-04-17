07b95d806277e160eccc1be6a66cf8a642324328
sylligardos <sylli1998@gmail.com>
2024-04-17 17:32:27 +0200

    Added more model selectors and saving the results per time series and not per dataset

--- src/main.py
+++ src/main.py
@@ -9,86 +9,76 @@
 import os
 import numpy as np
 import csv
+import pandas as pd
 
-from utils.config import raw_data_path, generate_window_data_path, model_parameters_file, weights_path, scores_path
-from utils.utils import load_timeseries, load_model_selector, predict_timeseries, combine_probabilities_average, combine_probabilities_vote, load_anomaly_scores, combine_anomaly_scores, compute_metrics, save_results_to_csv
-from models.model.convnet import ConvNet
+from utils.config import raw_data_path, generate_window_data_path, scores_path
+from utils.utils import \
+	load_data, load_model, predict_timeseries, compute_metrics, \
+	compute_weighted_scores
 from data.dataloader import Dataloader
 
 
-def run_experiment(k_values, combine_methods, selected_dataset_index=None):
-	window_size = 128
-	results = {method: [] for method in combine_methods}
+def run_experiment(k_values, combine_methods, selected_dataset_index, model_name, window_size):
+	results = []
 	window_data_path = generate_window_data_path(window_size)
-
-	# SKIP NASA OMG ...
 	dataloader = Dataloader(raw_data_path, window_data_path)
 	datasets = dataloader.get_dataset_names()
+	metric="AUC-PR"
+	
+	# SKIP NASA OMG ...
 	if "NASA" in datasets[selected_dataset_index]:
 		return f"Skipping {datasets[selected_dataset_index]}"
 
-	# Load the segmented time series and their ground truth, x - y
-	raw_timeseries, raw_anomalies, timeseries_names, window_timeseries = load_timeseries(raw_data_path, window_data_path, selected_dataset_index)
-	window_labels, window_timeseries = window_timeseries['label'], window_timeseries.drop('label', axis=1)
-	
-	# Load the 12 anomaly scores of the time series
-	scores, idx_failed = load_anomaly_scores(scores_path, timeseries_names)
-	if len(idx_failed) > 0:
-		df_indexes_to_delete = [timeseries_names[i] for i in idx_failed]
-		window_timeseries = window_timeseries[~window_timeseries.index.str.contains('|'.join(df_indexes_to_delete))]
-		for idx in sorted(idx_failed, reverse=True):
-			del timeseries_names[idx]
-			del raw_anomalies[idx]
-			del raw_timeseries[idx]
+	# Load the segmented time series, their ground truth, and their scores
+	raw_anomalies, timeseries_names, window_timeseries, scores = load_data(raw_data_path, window_data_path, selected_dataset_index, scores_path)
 
 	# Load a model selector (obviously window size between time series and model selector should match)
-	model = load_model_selector(
-		model=ConvNet, 
-		model_parameters_file=os.path.join(model_parameters_file, "convnet_default.json"), 
-		weights_path=os.path.join(weights_path, "convnet_default_128"), 
-		window_size=window_size
-	)
+	model = load_model(model_name, window_size)
 
 	# Get the 12 number vector that is the predicted weights for each detector
-	window_pred_probabilities = predict_timeseries(model, window_timeseries, timeseries_names)
+	window_pred_probabilities = predict_timeseries(model_name, model, window_timeseries, timeseries_names)
 
 	for k in k_values:
 		for combine_method in combine_methods:
-			# Compute Top-k weighted average of probabilities
-			if combine_method == 'average':
-				pred_probabilities = combine_probabilities_average(window_pred_probabilities, k)
-			elif combine_method == 'to_vote':
-				pred_probabilities = combine_probabilities_vote(window_pred_probabilities, k)
-			else:
-				raise ValueError("Invalid combine_method. Choose either 'average' or 'to_vote'.")
+			weighted_scores = compute_weighted_scores(window_pred_probabilities, scores, combine_method, k)
+			
+			# Compute the metric value of the combined anomaly score
+			metric_results = compute_metrics(raw_anomalies, weighted_scores)
+			results_df = pd.DataFrame(metric_results, timeseries_names, columns=[metric])
 
-			# Average them according to the weights
-			weighted_scores = combine_anomaly_scores(scores, pred_probabilities)
+			# Save the results dictionary to a CSV file
+			results_df.to_csv(os.path.join("reports", "results_04_2024", f"{datasets[selected_dataset_index]}_{model_name}{window_size}_{combine_method}_k{k}.csv"))
 
-			# Compute the metrics that you want with the combined anomaly score and the ground truth
-			results[combine_method].append(np.mean(compute_metrics(raw_anomalies, weighted_scores)))
-
-	# Save the results dictionary to a CSV file
-	save_results_to_csv(results, k_values, datasets[selected_dataset_index])
+			results.append({
+				"combine_method": combine_method,
+				"k": k,
+				"average_value": np.mean(metric_results),
+			})
 
 	return results
 
-	# Save the result in a csv, e.g.:
-	# Dataset | Time series | file name | window size | model selector | predicted weight AE | predicted weight NormA | ... | 
-	# save_results()
-
-
 
 def main():
-	# k_values = np.arange(1, 13)  # Different values of k
-	k_values = [1, 3, 5, 7]
-	combine_methods = ['average', 'to_vote']  # Different ways to combine the probabilities
-	selected_dataset_indexes = np.arange(13, 18)  # Index of the selected dataset
+	k_values = np.arange(1, 13)
+	combine_methods = ['average', 'vote']  # Different ways to combine the probabilities of the multiple windows per time series
+	selected_dataset_indexes = np.arange(0, 18)  # Index of the selected dataset
+	model_selectors = [("resnet", 1024), ("convnet", 128), ("sit", 512), ("rocket", 128), ("knn", 1024)]
+	model_idx = 2
 
-	for selected_dataset_index in selected_dataset_indexes:
-		results = run_experiment(k_values, combine_methods, selected_dataset_index)
-		print(results)
-			
+	for selected_dataset_index in [4]:
+		results = run_experiment(
+			k_values, 
+			combine_methods, 
+			selected_dataset_index, 
+			model_name=model_selectors[model_idx][0], 
+			window_size=model_selectors[model_idx][1])
+		
+	for result in results:
+		if result["combine_method"] == 'average':
+			print(result)
+	for result in results:
+		if result["combine_method"] == 'vote':
+			print(result)
 
 
 if __name__ == "__main__":
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -8,6 +8,10 @@
 
 from data.dataloader import Dataloader
 from data.scoreloader import Scoreloader
+from models.model.convnet import ConvNet
+from models.model.resnet import ResNetBaseline
+from models.model.sit import SignalTransformer
+from utils.config import model_parameters_file, weights_path
 
 import numpy as np
 import pandas as pd
@@ -20,7 +24,42 @@ from scipy.special import softmax
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from tqdm import tqdm
-import csv
+from datetime import datetime
+from pathlib import Path
+import pickle
+
+
+def load_data(raw_data_path, window_data_path, selected_dataset_index, scores_path):
+	"""
+	Load segmented time series, ground truth, anomaly scores, and related data.
+
+	Args:
+	- raw_data_path (str): Path to the raw data.
+	- window_data_path (str): Path to the window data.
+	- selected_dataset_index (int): Index of the selected dataset.
+	- scores_path (str): Path to the anomaly scores.
+
+	Returns:
+	- raw_anomalies (list): List of ground truth anomalies.
+	- timeseries_names (list): List of time series names.
+	- window_timeseries (DataFrame): DataFrame containing windowed time series.
+	- scores (array): Anomaly scores of the time series.
+	"""
+	# Load the segmented time series and their ground truth, x - y
+	raw_timeseries, raw_anomalies, timeseries_names, window_timeseries = load_timeseries(raw_data_path, window_data_path, selected_dataset_index)
+	window_labels, window_timeseries = window_timeseries['label'], window_timeseries.drop('label', axis=1)
+	
+	# Load the 12 anomaly scores of the time series
+	scores, idx_failed = load_anomaly_scores(scores_path, timeseries_names)
+	if len(idx_failed) > 0:
+		df_indexes_to_delete = [timeseries_names[i] for i in idx_failed]
+		window_timeseries = window_timeseries[~window_timeseries.index.str.contains('|'.join(df_indexes_to_delete))]
+		for idx in sorted(idx_failed, reverse=True):
+			del timeseries_names[idx]
+			del raw_anomalies[idx]
+			del raw_timeseries[idx]
+
+	return raw_anomalies, timeseries_names, window_timeseries, scores
 
 
 def load_timeseries(raw_data_path, window_data_path, selected_index=None):
@@ -55,40 +94,58 @@ def load_timeseries(raw_data_path, window_data_path, selected_index=None):
 	return x, y, fnames, df
 
 
-def load_model_selector(model, model_parameters_file, weights_path, window_size):
-	# Read models parameters
-	model_parameters = load_json(model_parameters_file)
+def load_anomaly_scores(path, fnames):
+	scoreloader = Scoreloader(path)
+
+	scores, idx_failed = scoreloader.load(fnames)
+
+	return scores, idx_failed
+
+
+def predict_timeseries(model_name, model, df, fnames):
+	"""
+    Predict anomalies in time series data using a specified model. 
+	Use deep learning model for prediction if the model is a deep learning model,
+    otherwise use classic model for prediction.
+
+    Args:
+    - model_name (str): Name of the model.
+    - model (object): Trained model object.
+    - df (DataFrame): DataFrame containing the time series data.
+    - fnames (list): List of file names corresponding to the time series data.
+
+    Returns:
+    - predictions (array): Predicted anomalies for the time series data.
+    """
+	return predict_deep(model, df, fnames) if is_deep_learning_model(model_name) else predict_classic(model, df, fnames)
+
+
+def is_deep_learning_model(model_name):
+	"""
+	Check if the model name corresponds to a deep learning model.
+
+	Args:
+	- model_name (str): Name of the model.
+
+	Returns:
+	- is_deep_learning (bool): True if the model is a deep learning model, False otherwise.
 	
-	# Change input size according to input
-	if 'original_length' in model_parameters:
-		model_parameters['original_length'] = window_size
-	if 'timeseries_size' in model_parameters:
-		model_parameters['timeseries_size'] = window_size
+	Raises:
+	- ValueError: If the model name is not recognized.
+	"""
+	deep_learning_models = ['convnet', 'resnet', 'sit']
+	classic_models = ['knn', 'rocket']
 
-	# Load model
-	model = model(**model_parameters)
-
-	# Check if weights_path is specific file or dir
-	if os.path.isdir(weights_path):
-		# Check the number of files in the directory
-		files = os.listdir(weights_path)
-		if len(files) == 1:
-			# Load the single file from the directory
-			weights_path = os.path.join(weights_path, files[0])
-		else:
-			raise ValueError("Multiple files found in the 'model_path' directory. Please provide a single file or specify the file directly.")
-
-	if torch.cuda.is_available():
-		model.load_state_dict(torch.load(weights_path))
-		model.eval()
-		model.to('cuda')
+	if model_name.lower() in deep_learning_models:
+		return True
+	elif model_name.lower() in classic_models:
+		return False
 	else:
-		model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
-		model.eval()
+		raise ValueError("Unrecognized model name. Please provide a valid model name.")
 
-	return model
 
-def predict_timeseries(model, df, fnames):
+
+def predict_deep(model, df, fnames):
 	preds = []
 	tensor_softmax = nn.Softmax(dim=1)
 	device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -102,6 +159,18 @@ def predict_timeseries(model, df, fnames):
 			curr_prediction = model(data_tensor)
 			curr_prediction = tensor_softmax(curr_prediction)
 		preds.append(curr_prediction.cpu().detach().numpy())
+
+	return preds
+
+def predict_classic(model, df, fnames):
+	preds = []
+	
+	# Compute predictions and inference time
+	for fname in tqdm(fnames, desc="Predicting", leave=False):
+		# Df to tensor
+		x = df.filter(like=fname, axis=0)
+		curr_prediction = model.predict(x.values)
+		preds.append(curr_prediction)
 
 	return preds
 
@@ -156,16 +225,6 @@ def combine_probabilities_vote(pred_probabilities, k):
 	return all_vote_probabilities
 
 
-def load_anomaly_scores(path, fnames):
-	scoreloader = Scoreloader(path)
-
-	scores, idx_failed = scoreloader.load(fnames)
-	# if len(idx_failed) > 0:
-	# 	raise ValueError("Some scores failed, take care")
-
-	return scores, idx_failed
-
-
 def combine_anomaly_scores(scores, weights):
 	all_weighted_scores = []
 
@@ -197,8 +256,37 @@ def combine_anomaly_scores(scores, weights):
 	return all_weighted_scores
 
 
+def compute_weighted_scores(window_pred_probabilities, scores, combine_method, k):
+	"""
+	Compute weighted anomaly scores for a given combine method and value of k.
+
+	Args:
+	- window_pred_probabilities (list of arrays): List of predicted probabilities for each window.
+	- scores (array): Anomaly scores for the time series.
+	- combine_method (str): Method to combine probabilities, either 'average' or 'vote'.
+	- k (int): Value of k for combining probabilities.
+
+	Returns:
+	- weighted_scores (array): Weighted anomaly scores computed using the specified combine method and value of k.
+
+	"""
+
+	# Combine the predicted probabilities from multiple windows into a single vector
+	if combine_method == 'average':
+		pred_probabilities = combine_probabilities_average(window_pred_probabilities, k)
+	elif combine_method == 'vote':
+		pred_probabilities = combine_probabilities_vote(window_pred_probabilities, k)
+	else:
+		raise ValueError("Invalid combine_method. Choose either 'average' or 'vote'.")
+
+	# Average the scores according to the weights
+	weighted_scores = combine_anomaly_scores(scores, pred_probabilities)
+
+	return weighted_scores
+
 
 def compute_metrics(labels, scores):
+	
 	all_results = []
 
 	for label, score in zip(labels, scores):
@@ -207,16 +295,6 @@ def compute_metrics(labels, scores):
 		all_results.append(result)
 
 	return all_results
-
-def save_results_to_csv(results, k_values, dataset):
-	filename = f"{dataset}.csv"
-	with open(filename, 'w', newline='') as csvfile:
-		fieldnames = ['Combine Method'] + [f'k={k}' for k in k_values]
-		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-
-		writer.writeheader()
-		for combine_method, result_list in results.items():
-			writer.writerow({**{'Combine Method': combine_method}, **{f'k={k}': result for k, result in zip(k_values, result_list)}})
 
 
 def load_json(file_path):
@@ -242,3 +320,135 @@ def load_json(file_path):
 	except Exception as e:
 		print(f"Error loading JSON: {e}")
 		return None
+
+
+def load_model(model_name, window_size):
+	"""
+	This function loads a machine learning model based on the specified model name and window size. It supports both deep learning models (e.g., ConvNet, ResNet) and non-deep models (e.g., KNN, ROCKET). It dynamically selects the appropriate loading method based on the model type.
+	
+	Args:
+	- model_name (str): Name of the model to load.
+	- window_size (int): Size of the window.
+
+	Returns:
+	- model: Loaded machine learning model.
+	"""
+	deep_models = ['convnet', 'resnet', 'sit']
+	classic_models = ['knn', 'rocket']
+
+	if model_name in deep_models:
+		model_selector_dict = get_model_selector_dict(model_name, window_size)
+		model = load_deep_model(**model_selector_dict)
+	elif model_name in classic_models:
+		model = load_classic_model(path=os.path.join(weights_path, f"{model_name}_{window_size}"))
+	else:
+		raise ValueError(f"Model name \"{model_name}\" not expected")
+	return model
+
+
+def load_deep_model(model, model_parameters_file, weights_path, window_size):
+	"""
+	This function loads a deep learning model (e.g., ConvNet, ResNet) based on the provided model parameters file and weights path. It also adjusts the model's input size according to the specified window size. The function handles loading the model weights, considering whether CUDA is available or not.
+	
+	Args:
+	- model (nn.Module): Deep learning model class.
+	- model_parameters_file (str): File path to the model parameters JSON file.
+	- weights_path (str): Directory or file path containing the model weights.
+	- window_size (int): Size of the window.
+
+	Returns:
+	- model: Loaded deep learning model.
+	"""
+	# Read models parameters
+	model_parameters = load_json(model_parameters_file)
+	
+	# Change input size according to input
+	if 'original_length' in model_parameters:
+		model_parameters['original_length'] = window_size
+	if 'timeseries_size' in model_parameters:
+		model_parameters['timeseries_size'] = window_size
+
+	# Load model
+	model = model(**model_parameters)
+
+	# Check if weights_path is specific file or dir
+	if os.path.isdir(weights_path):
+		# Check the number of files in the directory
+		files = os.listdir(weights_path)
+		if len(files) == 1:
+			# Load the single file from the directory
+			weights_path = os.path.join(weights_path, files[0])
+		else:
+			raise ValueError("Multiple files found in the 'model_path' directory. Please provide a single file or specify the file directly.")
+
+	if torch.cuda.is_available():
+		model.load_state_dict(torch.load(weights_path))
+		model.eval()
+		model.to('cuda')
+	else:
+		model.load_state_dict(torch.load(weights_path, map_location=torch.device('cpu')))
+		model.eval()
+
+	return model
+
+
+def get_model_selector_dict(model_name, window_size):
+	"""
+	Return a dictionary containing information for the model selector.
+
+	Args:
+	- model_name (str): Name of the model ('convnet', 'resnet', or 'sit').
+	- window_size (int): Size of the window.
+
+	Returns:
+	- model_selector_dict (dict): Dictionary containing information for the model selector.
+	"""
+	model_parameters = {
+		'convnet': {
+			'model': ConvNet,
+			'model_parameters_file': os.path.join(model_parameters_file, 'convnet_default.json'),
+			'weights_path': os.path.join(weights_path, 'convnet_default_' + str(window_size)),
+			'window_size': window_size
+		},
+		'resnet': {
+			'model': ResNetBaseline,
+			'model_parameters_file': os.path.join(model_parameters_file, 'resnet_default.json'),
+			'weights_path': os.path.join(weights_path, 'resnet_default_' + str(window_size)),
+			'window_size': window_size
+		},
+		'sit': {
+			'model': SignalTransformer,
+			'model_parameters_file': os.path.join(model_parameters_file, 'sit_stem_original.json'),
+			'weights_path': os.path.join(weights_path, 'sit_stem_original_' + str(window_size)),
+			'window_size': window_size
+		}
+	}
+
+	return model_parameters.get(model_name.lower())
+
+
+def load_classic_model(path):
+	"""
+	This function loads a non-deep learning model (e.g., KNN, ROCKET) from a pickle (.pkl) file. 
+	If the provided path is a directory, it loads the latest model within that directory based on the creation date.
+	
+	Args:
+	- path (str): Path to the specific classifier to load or the directory containing classifiers.
+	
+	Returns:
+	- output: Loaded non-deep learning model.
+	"""
+
+	# If model is not given, load the latest
+	if os.path.isdir(path):
+		models = [x for x in os.listdir(path) if '.pkl' in x]
+		models.sort(key=lambda date: datetime.strptime(date, 'model_%d%m%Y_%H%M%S.pkl'))
+		path = os.path.join(path, models[-1])
+	elif '.pkl' not in path:
+		raise ValueError(f"Can't load this type of file {path}. Only '.pkl' files please")
+
+	filename = Path(path)
+	with open(f'{filename}', 'rb') as input:
+		output = pickle.load(input)
+	
+	return output
